e08f9f350a8730a32641774c770abf1301ab8121
Martin Nenseth <martin.nenseth@gmail.com>
2017-01-25 14:27:02 +0100

    structuring project

---
+++ HelloWorld/log/log.go
@@ -0,0 +1,11 @@
+package log
+
+import (
+	//"log"
+	"math/rand"
+)
+
+func lol(x, y int){
+	rand.Intn(x)
+	rand.Intn(y)
+}
---
+++ HelloWorld/log/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+
+
+func main() {
+
+	for i := 0; i < 10; i++ {
+		lol(2000, 300)
+		fmt.Println(lol)
+	}
+
+
+}
+
